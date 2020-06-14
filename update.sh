83029cde63c9f673a20e09e89ea4a4d94ce5ef98
Alan M Silva <alanfortlink@gmail.com>
2020-06-14 22:55:46 +0100

    update

--- update.sh
+++ update.sh
@@ -8,3 +8,8 @@ cd projetos
 git clone https://github.com/alanfortlink/ccr-wppui.git
 git clone https://github.com/alanfortlink/ccr-service.git
 git clone https://github.com/alanfortlink/ccr-webui.git
+
+cd ..
+git add projetos
+git commit -m "Atualizado em $(date)"
+git push
