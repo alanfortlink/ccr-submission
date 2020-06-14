6834ccf0095f97f9282f5af4f9c72e45ab173618
Alan M Silva <alanfortlink@gmail.com>
2020-06-14 22:53:29 +0100

    update

--- update.sh
+++ update.sh
@@ -2,6 +2,9 @@
 
 rm -rf *
 
+mkdir projetos
+cd projetos
+
 git clone https://github.com/alanfortlink/ccr-wppui.git
 git clone https://github.com/alanfortlink/ccr-service.git
 git clone https://github.com/alanfortlink/ccr-webui.git
