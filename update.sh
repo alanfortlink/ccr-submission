80f1868db17948d1b28067d02d5f9fa7276927ba
Alan M Silva <alanfortlink@gmail.com>
2020-06-14 23:09:56 +0100

    update

--- update.sh
+++ update.sh
@@ -8,12 +8,7 @@ cd projetos
 repos="ccr-wppui ccr-service ccr-webui"
 
 for repo in $repos; do
-    git clone https://github.com/alanfortlink/$repo.git
-done
-
-for repo in $repos; do
-    rm -rf projetos/$repo/.git
-    echo "rm -rf projetos/$repo/.git"
+    git clone https://github.com/alanfortlink/$repo.git && rm -rf projetos/$repo/.git
 done
 
 cd ..
